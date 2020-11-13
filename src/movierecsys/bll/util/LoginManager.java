ac559883b49c2b46b02496b95baccfb4f014052d
Snoozy <snoozy1995@gmail.com>
2020-11-13 15:09:22 +0100

    LoginManager basic implemented.

--- src/movierecsys/bll/util/LoginManager.java
+++ src/movierecsys/bll/util/LoginManager.java
@@ -1,4 +1,20 @@
 package movierecsys.bll.util;
 
+import movierecsys.be.User;
+
 public class LoginManager {
+    private static User loggedIn=null;
+
+    public static void loginUser(User user){
+        loggedIn=user;
+    }
+    public static void logout(){
+        loggedIn=null;
+    }
+    public static boolean isLoggedIn(){
+        return (loggedIn!=null);
+    }
+    public static boolean isLoggedIn(User user){
+        return (loggedIn==user);
+    }
 }
