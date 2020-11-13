f9a6c9a484c173f0679b2b761daf35897f6b4ec5
Snoozy420 <snoozy1995@gmail.com>
2020-11-13 10:09:57 +0100

    Warning cleanup.

--- src/movierecsys/bll/util/MovieManager.java
+++ src/movierecsys/bll/util/MovieManager.java
@@ -2,24 +2,27 @@ package movierecsys.bll.util;
 
 import movierecsys.be.Movie;
 import movierecsys.dal.MovieDAO;
+import movierecsys.dal.RatingDAO;
+import movierecsys.dal.UserDAO;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MovieManager {
-    private MovieSearcher movSearcher=new MovieSearcher();
+    private final MovieSearcher movSearcher=new MovieSearcher();
 
     public MovieManager(){
-        try{ MovieDAO.getAllMovies(); }
-        catch(Exception e){ }
+        MovieDAO.getAllMovies();
+        UserDAO.getAllUsers();
+        RatingDAO.getAllRatings();
     }
 
     public List<Movie> searchMovie(String query){
-        return movSearcher.search(MovieDAO.moviesInMemory,query);
+        return movSearcher.search(MovieDAO.getAllMovies(),query);
     }
 
     public List<String> toListString(List<Movie> moviesList){
-        List<String> moviesString=new ArrayList<String>();
+        List<String> moviesString=new ArrayList<>();
         for(Movie movie:moviesList){
             moviesString.add("["+movie.getId()+"] "+movie.getTitle()+" ("+movie.getYear()+")");
         }
